3ba07e224d2f8a9b5544ac0f624302c0ae8dea9b
Hussein Kadiri <hkadiri@mobitv.com>
2013-09-10 22:07:39 -0700

    updating

--- backend.py
+++ backend.py
@@ -1,8 +1,16 @@
 import os
 from flask import Flask
-
+import soundcloud
+import json
+CLIENT_ID = '707228f02dd69922dbac5f0bb309a07c'
+client = soundcloud.Client(client_id=CLIENT_ID)
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+@app.route('/api/<query>')
+def hello(query):
+    tracks = client.get('/tracks', q=query, filter='downloadable')
+    js = {}
+    for t in tracks:
+        try:
+            js[t.title] = "{}?client_id={}".format(t.download_url, CLIENT_ID)
+    return json.dumps(js)
