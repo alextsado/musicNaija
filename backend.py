3bc85a0802cc95f12ea7f98842efd6b9e7da880b
Hussein Kadiri <hkadiri@mobitv.com>
2013-09-10 22:40:17 -0700

    using k=v arg variable

--- backend.py
+++ backend.py
@@ -1,18 +1,18 @@
 import os
-from flask import Flask
+from flask import Flask, request
 import soundcloud
 import json
 CLIENT_ID = '707228f02dd69922dbac5f0bb309a07c'
 client = soundcloud.Client(client_id=CLIENT_ID)
 app = Flask(__name__)
 
-@app.route('/api?q=<query>')
-def hello(query):
-    tracks = client.get('/tracks', q=query, filter='downloadable')
+@app.route('/api',methods=['GET'])
+def hello():
+    tracks = client.get('/tracks', q=request.form['q'], filter='downloadable')
     js = {}
     for t in tracks:
         try:
             js[t.title] = {'download_url': "{}?client_id={}".format(t.download_url, CLIENT_ID), 'stream_url': "{}?client_id={}".format(t.stream_url, CLIENT_ID)}
         except AttributeError:
             continue
-    return json.dumps(js, indent=2)
+    return json.dumps(js)
